90b126dde9fdbd332a20207ee9ded4a21ff9907b
Ashuya2003 <128083527+Ashuya2003@users.noreply.github.com>
2023-09-11 18:02:12 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,16 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> color = "Yellow"
+>>> print(color)
+Yellow
+>>> variables_are_cool = 2345 * 7323
+>>> print(variables_are_cool)
+17172435
+>>> result = 2 * 3
+>>> print(result)
+6
+>>> age = int(input("Please enter your age: "))
+Please enter your age: 10
+>>> age += 10
+>>> print("In 10 years, you will be " + str(age) + " years old.")
+In 10 years, you will be 20 years old.
---
+++ task2.py
@@ -0,0 +1,13 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> A = int(input("enter the value of A : "))
+enter the value of A : 3
+>>> B = int(input("enter the value of B : "))
+enter the value of B : 5
+>>> # Calculate and print the sum
+... print(A + B)
+8
+>>> print(A - B)
+-2
+>>> print(A * B)
+15
